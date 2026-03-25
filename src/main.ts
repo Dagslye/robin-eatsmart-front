8d097e4cbcdbc3ee96c0050d6c491195c70b98ce
Dagslye <robinlion13@gmail.com>
2026-03-25 13:49:22 +0100

    V1 ig

--- src/main.ts
+++ src/main.ts
@@ -20,5 +20,47 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </p>
   </div>
 `
+// Fichier main.ts
+import './style.css'
+interface Plat {
+  id: number;
+  nom: string;
+  prix: number;
+  description?: string; 
+}
+
+const Pizzaanchnoi23cm: Plat = {
+  id: 1,
+  nom: "Pizza aux anchois, 23cm",
+  prix: 12.5
+};
+  const Pizzaanchnoi33cm: Plat = {
+  id: 1,
+  nom: "Pizza aux anchois, 33cm",
+  prix: 12.5
+};
+  const Pizzaemental23cm: Plat = {
+  id: 1,
+  nom: "Pizza emental, 23cm",
+  prix: 12.5
+};
+
+const appDiv = document.querySelector<HTMLDivElement>('#app');
+
+const produits: Plat[] = [Pizzaanchnoi23cm, Pizzaanchnoi33cm, Pizzaemental23cm];
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (appDiv) {
+  const listeHTML = produits.map(p => `
+    <div class="card">
+      <h3 class="menu-container">${p.nom}</h3>
+      <p class="card">${p.description ?? 'Pas de description'}</p>
+      <p class=""><strong>${p.prix}€</strong></p>
+    </div>
+  `).join('');
+
+    appDiv.innerHTML = `
+    <h1>Eatsmart - Carte du réstaurant (${produits.length} plats)</h1>
+    ${listeHTML}
+  `;
+}
